bd1078a5cdebd33c41bd85b89f26f6c98ad82c2d
Sébastien Lengagne <lengagne@gmail.com>
2020-08-29 15:22:32 +0200

    make creation of marker stl from file via openscad

--- apps/main.cpp
+++ apps/main.cpp
@@ -1,8 +1,75 @@
 #include <iostream>
+#include <fstream>
+
+
+std::string  GetBaseFilename(const std::string filename)
+{
+    size_t pos = filename.rfind(std::string("."));
+    if(pos == std::string::npos)  //No extension.
+        return filename;
+
+    if(pos == 0)    //. is at the front. Not an extension.
+        return filename;
+
+    return filename.substr(0, pos);
+}
+
+using namespace std;
 
 int main( int argc, char ** argv)
 {
-    std::cout<<"Image to stl converter"<<std::endl;
+    std::string filename,text;
+    bool print_text = false;
+    if (argc == 2)
+    {
+        filename = argv[1];
+        std::cout<<"Using filename " << filename <<std::endl;
+    }else if (argc==3)
+    {
+        filename = argv[1];
+        text = argv[2];
+        print_text = true;
+        std::cout<<"Using filename " << filename <<" With text: "<< text<<std::endl;
+        
+    }else
+    {
+        std::cerr<<"Error you must specify a file name " << std::endl;
+        return 1;
+    }
     
+    double width = 1.0;
+    double length = 1.0;
+    double depth = 5e-3;
+    
+    std::cout<<"Image to stl converter using openscad"<<std::endl;
+    
+    // creation du fond black
+    ofstream fileblack;
+    fileblack.open ("black.scad");
+    fileblack << " difference() \n {    \n translate([5,0,0])  cube([35,35,5],center=true); \n    translate([3,0,2.6])    scale([0.14,0.14,-1])  \n surface(file = \""<<filename<<"\", center = true);";
+    if (print_text)
+    {
+            fileblack<<"\ntranslate([21.5,-"<< text.size()<<",-2.5])   rotate([0,0,90])  linear_extrude(6)  text (\""<<text<<"\",size=3);";
+    }
+    fileblack<<"\n } " <<std::endl;
+    fileblack.close();
+
+    ofstream filewhite;
+    filewhite.open ("white.scad");
+    if(print_text)  filewhite<<"union(){ \n translate([21.5,-"<< text.size()<<",-2.5])   rotate([0,0,90])  linear_extrude(5)  text (\""<<text<<"\",size=3); \n";
+    filewhite << " intersection() \n {    \n cube([35,35,5],center=true); \n    translate([3,0,2.6])    scale([0.14,0.14,-1])  \n surface(file = \""<<filename<<"\", center = true); \n } " <<std::endl;
+    if(print_text)  filewhite<<"} \n";
+    filewhite.close();
+    
+    std::cout<<"running openscad for black file"<<std::endl;
+    system("openscad -o black.stl black.scad");
+    std::cout<<"running openscad for white file"<<std::endl;
+    system("openscad -o white.stl white.scad");    
+    
+    std::string dummy = "cp black.stl " + GetBaseFilename(filename)+"_black.stl";
+    system(dummy.c_str());
+    dummy = "cp white.stl " + GetBaseFilename(filename)+"white.stl";
+    system(dummy.c_str());
+        
     return 0;
 }
